412935588c88731e3b9539e24428ea49e1cbf6ec
Dmitry Pavlovich <dimonza49@gmail.com>
2024-05-14 18:27:35 +0300

    new rarity

--- utils/helpers.js
+++ utils/helpers.js
@@ -5,13 +5,13 @@ function getRandomEgg() {
     const eggsArr = eggs.eggs;
 
     if (randomNumber <= 65) {
-        randomNumber = Math.floor(Math.random() * 3) + 1;
+        randomNumber = Math.floor(Math.random() * 5) + 1;
 
-        if (randomNumber === 1) {
+        if (randomNumber <= 2) {
             return eggsArr[0];
-        } else if (randomNumber === 2) {
+        } else if (randomNumber <= 4) {
             return eggsArr[1];
-        } else if (randomNumber === 3) {
+        } else if (randomNumber === 5) {
             return eggsArr[2];
         }
     } else if (randomNumber <= 88) {
