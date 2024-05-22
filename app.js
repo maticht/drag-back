214d4749d616eea6d51816b390762b103e333d23
maticht <maticht.1999@gmail.com>
2024-05-23 01:01:24 +0200

    handleNewEggRarity

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const getUserData = require("./bot/serverRequests/user/getUserData");
 const getAllUsers = require("./bot/serverRequests/user/getAllUsers");
+const alchemistUpgrade = require("./bot/serverRequests/user/alchemistUpgrade");
 const updateHummer = require("./bot/serverRequests/updateHummer");
 const updateBarrel = require("./bot/serverRequests/barrel/updateBarrel");
 const barrelExpectation = require("./bot/serverRequests/barrel/barrelExpectation");
@@ -29,6 +30,7 @@ app.use('/api', router);
 app.use("/getUserData", getUserData);
 app.use("/updateHummer", updateHummer);
 app.use("/updateBarrel", updateBarrel);
+app.use("/alchemistUpgrade", alchemistUpgrade)
 app.use("/barrelExpectation", barrelExpectation);
 app.use("/collectionBarrel", collectionBarrel);
 app.use("/getAllUsers", getAllUsers);
