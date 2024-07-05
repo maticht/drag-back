c83409985d0e0b23887c6acdbfc92519d8afb31f
Dmitry Pavlovich <dimonza49@gmail.com>
2024-07-05 20:47:35 +0300

    top mini game screen

--- bot/controller/userController.js
+++ bot/controller/userController.js
@@ -40,7 +40,7 @@ class UserController {
                 // username: users[userIndex].username
             };
 
-            res.status(200).send({ users, currentUser });
+            res.status(200).send({ users: users.slice(0, 1000), currentUser });
         } catch (error) {
             console.error(error);
             res.status(500).send({ message: "Internal Server Error" });
@@ -89,99 +89,40 @@ class UserController {
                 username: users[userIndex].username
             };
 
-            res.status(200).send({ users, currentUser });
+            res.status(200).send({ users: users.slice(0, 1000), currentUser });
         } catch (error) {
             console.error(error);
             res.status(500).send({ message: "Internal Server Error" });
         }
     }
 
-    // async getUserTopPlace(req, res) {
-    //     try {
-    //         const userId = req.params.userId;
-    //
-    //         const result = await User.aggregate([
-    //             {
-    //                 $match: { chatId: userId }
-    //             },
-    //             {
-    //                 $lookup: {
-    //                     from: 'users',
-    //                     let: { score: "$overallScore" },
-    //                     pipeline: [
-    //                         {
-    //                             $match: {
-    //                                 $expr: {
-    //                                     $gt: ["$overallScore", "$$score"]
-    //                                 }
-    //                             }
-    //                         },
-    //                         {
-    //                             $count: "rank"
-    //                         }
-    //                     ],
-    //                     as: 'rank'
-    //                 }
-    //             },
-    //             {
-    //                 $addFields: {
-    //                     userTopPlace: {
-    //                         $add: [{ $arrayElemAt: ["$rank.rank", 0] }, 1]
-    //                     }
-    //                 }
-    //             },
-    //             {
-    //                 $project: {
-    //                     chatId: 1,
-    //                     overallScore: 1,
-    //                     userTopPlace: 1
-    //                 }
-    //             }
-    //         ]);
-    //
-    //         if (result.length === 0) {
-    //             return res.status(404).send({ message: "User not found" });
-    //         }
-    //
-    //         const user = result[0];
-    //
-    //         // Обновляем поле userTopPlace в документе пользователя
-    //         await User.updateOne({ chatId: userId }, { userTopPlace: user.userTopPlace });
-    //
-    //         return res.json({ user });
-    //     } catch (error) {
-    //         console.error(error);
-    //         res.status(500).send({ message: "Internal Server Error" });
-    //     }
-    // }
-
-    async getUserTopPlace(req, res) {
-        const session = await mongoose.startSession();
-        session.startTransaction();
+    async getAllUsersForMiniGameTop(req, res) {
         try {
-            const user = await User.findOne({ chatId: req.params.userId }, 'username userTopPlace score overallScore eggs').session(session);
 
-            if (!user) {
-                await session.abortTransaction();
-                session.endSession();
+            const currentUserId = req.params.userId;
+
+            const users = await User.find({}, 'miniGame.dailyBestScore username chatId').sort({ "miniGame.dailyBestScore": -1 });
+
+            const scoreRewards = rewardsTemplateData.weeklyScoreRewards;
+
+            const userIndex = users.findIndex(user => user.chatId.toString() === currentUserId);
+
+            if (userIndex === -1) {
                 return res.status(404).send({ message: "User not found" });
             }
 
-            const higherScoreCount = await User.countDocuments({ overallScore: { $gt: user.overallScore } }).session(session);
+            const placeInTop = userIndex + 1;
 
-            const userTopPlace = higherScoreCount + 1;
+            const userLeague = scoreRewards.find(r => placeInTop >= r.placeInTop[0] && placeInTop <= r.placeInTop[1]);
 
-            user.userTopPlace = userTopPlace;
-            await user.save({ session });
+            const currentUser = {
+                placeInTop: placeInTop,
+                league: userLeague ? userLeague.league : 'Unranked',
+            };
 
-            await session.commitTransaction();
-            session.endSession();
-
-            return res.json({ user });
+            res.status(200).send({ users: users.slice(0, 1000), currentUser });
         } catch (error) {
-            await session.abortTransaction();
-            session.endSession();
-            console.error('Error fetching user top place:', error);
+            console.error(error);
             res.status(500).send({ message: "Internal Server Error" });
         }
     }
--- bot/routes/userRouter.js
+++ bot/routes/userRouter.js
@@ -5,6 +5,7 @@ const userController = require("../controller/userController");
 router.get('/data/:userId', userController.getUserData);
 router.get('/scoreTop/:userId', userController.getAllUsersForScoreTop);
 router.get('/referralTop/:userId', userController.getAllUsersForReferralTop);
+router.get('/miniGameTop/:userId', userController.getAllUsersForMiniGameTop);
 router.put('/updateWalletHash/:userId', userController.updateWalletHash)
 router.put('/updateProfileLevel/:userId', userController.updateProfileLevel)
 
