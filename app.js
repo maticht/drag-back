24f2404b1254e77465e10159abc231e50516f3ce
Dmitry Pavlovich <dimonza49@gmail.com>
2024-07-10 03:12:16 +0300

    cors options off

--- app.js
+++ app.js
@@ -22,16 +22,16 @@ async function initializeApp() {
         await checkConnection();
 
         app.use(express.json());
-        const allowedOrigins = ['https://oyster-app-4mimt.ondigitalocean.app'];
-        app.use(cors({
-            origin: function (origin, callback) {
-                if (!origin || allowedOrigins.includes(origin)) {
-                    callback(null, true);
-                } else {
-                    callback(new Error('Not allowed by CORS'));
-                }
-            }
-        }));
+        // const allowedOrigins = ['https://oyster-app-4mimt.ondigitalocean.app'];
+        // app.use(cors({
+        //     origin: function (origin, callback) {
+        //         if (!origin || allowedOrigins.includes(origin)) {
+        //             callback(null, true);
+        //         } else {
+        //             callback(new Error('Not allowed by CORS'));
+        //         }
+        //     }
+        // }));
 
         app.use(cors());
         app.use('/api', router);
