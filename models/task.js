0ed31c86ca27d7a831dc7d639c6df06985f1e83e
Dmitry Pavlovich <dimonza49@gmail.com>
2024-07-20 01:20:54 +0300

    task localization

--- models/task.js
+++ models/task.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose');
 
+const translationSchema = new mongoose.Schema({
+    en: { type: String, required: false },
+    ru: { type: String, required: false },
+    ua: { type: String, required: false },
+    pt: { type: String, required: false },
+    es: { type: String, required: false },
+}, { _id: false });
+
 const taskSchema = new mongoose.Schema({
-    title: {type:String, required:false},
-    description: {type:String, required:false },
-    reward: {type:Number, required:false },
-    link: {type:String, required:false},
-    checkable: {type:Boolean, required:false},
-}, {toJSON: {virtuals: true}});
+    title: { type: translationSchema, required: false },
+    description: { type: translationSchema, required: false },
+    reward: { type: Number, required: false },
+    link: { type: String, required: false },
+    checkable: { type: Boolean, required: false },
+}, { toJSON: { virtuals: true } });
 
 const Task = mongoose.model("Task", taskSchema);
 
-module.exports = {Task};
+module.exports = { Task };
